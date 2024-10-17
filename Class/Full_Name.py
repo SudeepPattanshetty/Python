1c3eb17996d76ccf294e5f67ee73ef83d576ca1d
Sudeep <93774921+SudeepPattanshetty@users.noreply.github.com>
2024-10-17 15:31:10 +0530

    Concatenation of Strings

---
+++ Class/Full_Name.py
@@ -0,0 +1,6 @@
+fname = input("Enter first name: ")
+mname = input("Enter middle name: ")
+lname = input("Enter last name: ")
+
+fullName = fname + " " + mname + " " + lname
+print("\nFullName: ", fullName)
